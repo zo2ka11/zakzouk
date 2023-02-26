ed4b605687fc1c7f537e290cfd650782affb9de5
zo2ka11 <mina.zakzouk1@gmail.com>
2023-02-27 00:04:58 +0200

    Udated

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["","Software Engineer", "Flutter Develpoer", "SoftWare Testing", "....."],
+    strings:["","Software Engineer", "Flutter Developer", "SoftWare Testing", "Data scientist", "....."],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
